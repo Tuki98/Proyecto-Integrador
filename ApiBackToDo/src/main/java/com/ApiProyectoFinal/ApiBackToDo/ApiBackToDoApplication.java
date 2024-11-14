307047f67146c1d8001fbe3c17b8cda6acae1587
Tuki98 <oscar.aquise@hotmail.com>
2024-11-14 01:34:27 -0500

    Añadido Proyecto Vacio

---
+++ ApiBackToDo/src/main/java/com/ApiProyectoFinal/ApiBackToDo/ApiBackToDoApplication.java
@@ -0,0 +1,13 @@
+package com.ApiProyectoFinal.ApiBackToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBackToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBackToDoApplication.class, args);
+	}
+
+}
---
+++ ApiBackToDo/src/test/java/com/ApiProyectoFinal/ApiBackToDo/ApiBackToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiProyectoFinal.ApiBackToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
